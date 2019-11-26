1c58ef358d0470d5cafc7cc4c6b597295f4a0577
sophjac <33553357+sophjac@users.noreply.github.com>
2019-11-26 13:27:06 -0500

    Add files via upload

--- js/challenge5.js
+++ js/challenge5.js
@@ -1,24 +1,44 @@
 console.log("here")
-var imgs = document.querySelectorAll("img");
+    // var imgs = document.querySelectorAll("img");
 var msg = "Hover over an image below."
-for (let i = 0; i < imgs.length;i++){
-	imgs[i].onmouseover = function() {
-	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
-	 	document.querySelector('#image').innerHTML= imgs[i].alt;
-	 }
+    // for (let i = 0; i < imgs.length;i++){
+    // 	imgs[i].onmouseover = function() {
+    // 	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
+    // 	 	document.querySelector('#image').innerHTML= imgs[i].alt;
+    // 	 }
 
-	 imgs[i].onfocus = function() {
-	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
-	 	document.querySelector('#image').innerHTML= imgs[i].alt;
-	 }
+// 	 imgs[i].onfocus = function() {
+// 	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
+// 	 	document.querySelector('#image').innerHTML= imgs[i].alt;
+// 	 }
 
-	 imgs[i].onmouseleave = function() {
-	 	document.querySelector('#image').style.backgroundImage="url('')";
-	 	document.querySelector('#image').innerHTML= msg;
-	 }
+// 	 imgs[i].onmouseleave = function() {
+// 	 	document.querySelector('#image').style.backgroundImage="url('')";
+// 	 	document.querySelector('#image').innerHTML= msg;
+// 	 }
 
-	 imgs[i].onblur = function() {
-	 	document.querySelector('#image').style.backgroundImage="url('')";
-	 	document.querySelector('#image').innerHTML= msg;
-	 }
-}
+// 	 imgs[i].onblur = function() {
+// 	 	document.querySelector('#image').style.backgroundImage="url('')";
+// 	 	document.querySelector('#image').innerHTML= msg;
+// 	 }
+// }
+
+$("img").mouseover(function() {
+    $("#image").css("backgroundImage", "url('" + this.src + "')");
+    $("#image").html(this.alt);
+});
+
+$("img").focus(function() {
+    $("#image").css("backgroundImage", "url('" + this.src + "')");
+    $("#image").html(this.alt);
+});
+
+$("img").mouseleave(function() {
+    $("#image").css("backgroundImage", "url('')");
+    $("#image").html(msg);
+});
+
+$("img").blur(function() {
+    $("#image").css("backgroundImage", "url('')");
+    $("#image").html(msg);
+});

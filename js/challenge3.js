59a06dc3581c8407ec46ac84898af388a9a01710
sophjac <33553357+sophjac@users.noreply.github.com>
2019-11-26 00:46:09 -0500

    Add files via upload

--- js/challenge3.js
+++ js/challenge3.js
@@ -1,67 +1,8 @@
-// ch3form.onsubmit = function() {
-//     var fruit = document.querySelectorAll('input[name="fruit"]')
-//     for (let i = 0; i < fruit.length;i++){
-//         if (fruit[i].checked)
-//             return true;
-//     }
-//     alert("You must pick a fruit!")
-//     return false;
-
-// Advanced Answer
-// if(document.querySelector('input[name="fruit"]:checked'))
-//     return true;
-
-// alert("You must pick a fruit!")
-// return false;
-//   }
-
-// find a way to iterate
-//this is so inefficient (sorry drew)
-var val1 = 0;
-$("#kum").click(function() {
-    val1 = 1;
-});
-$("#figs").click(function() {
-    val1 = 1;
-});
-$("#straw").click(function() {
-    val1 = 1;
-});
-$("#blue").click(function() {
-    val1 = 1;
-});
-$("#ofrui").click(function() {
-    val1 = 1;
-});
-var val2 = 0;
-$("#freshman").click(function() {
-    val2 = 1;
-});
-$("#soph").click(function() {
-    val2 = 1;
-});
-$("#jr").click(function() {
-    val2 = 1;
-});
-$("#sr").click(function() {
-    val2 = 1;
-});
-$("#grad").click(function() {
-    val2 = 1;
-});
-$("#other").click(function() {
-    val2 = 1;
-});
-
-
 $("form").submit(function() {
-    if (val1 == 0) {
-        alert("You must pick a fruit!");
-        return false;
-    } else if (val2 == 0) {
-        alert("You must pick a class standing!");
-        return false;
-    } else {
+    if ($('input[name="fruit"]:checked').val() > 0 & $('input[name="standing"]:checked').val() > 0) {
         return true;
+    } else {
+        alert("You must pick from both columns");
+        return false;
     }
 });

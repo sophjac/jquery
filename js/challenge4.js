3ea40c8ab795f8cf9311a40b4b5f42f83bb29c30
sophjac <33553357+sophjac@users.noreply.github.com>
2019-11-26 01:35:30 -0500

    Add files via upload

--- js/challenge4.js
+++ js/challenge4.js
@@ -1,25 +1,48 @@
-ch4form.onsubmit = function() {
-    var validInput = true;
-    var name = document.querySelector("#fullname")
-    var address = document.querySelector("#streetaddr")
-    console.log("Here");
-    console.log(name);
-    console.log(address)
-    if (name.value.length == 0){
-        document.querySelector("#nameerrormsg").style.display = "block"; 
-        validInput = false;
+// ch4form.onsubmit = function() {
+//     var validInput = true;
+//     var name = document.querySelector("#fullname")
+//     var address = document.querySelector("#streetaddr")
+//     console.log("Here");
+//     console.log(name);
+//     console.log(address)
+//     if (name.value.length == 0) {
+//         document.querySelector("#nameerrormsg").style.display = "block";
+//         validInput = false;
+//     } else {
+//         document.querySelector("#nameerrormsg").style.display = "none";
+//     }
+
+//     if (address.value.length == 0) {
+//         document.querySelector("#addrerrormsg").style.display = "block";
+//         validInput = false;
+//     } else {
+//         document.querySelector("#addrerrormsg").style.display = "none";
+//     }
+
+//     return validInput;
+// }
+
+var name_ok = 0;
+var addr_ok = 0;
+
+$("form").submit(function() {
+    if ($("#fullname").val() == "") {
+        $("#nameerrormsg").show();
+        name_ok = 1;
+    } else {
+        $("#nameerrormsg").hide();
+        name_ok = 0;
     }
-    else {
-        document.querySelector("#nameerrormsg").style.display = "none";
+    if ($("#streetaddr").val() == "") {
+        $("#addrerrormsg").show();
+        addr_ok = 1;
+    } else {
+        $("#addrerrormsg").hide();
+        addr_ok = 0;
     }
 
-    if (address.value.length == 0){
-        document.querySelector("#addrerrormsg").style.display = "block"; 
-        validInput = false;
-    }
-    else {
-        document.querySelector("#addrerrormsg").style.display = "none"; 
-    }
+    if (name_ok == 1 | addr_ok == 1) {
+        return false;
+    } else { return true; }
 
-    return validInput;
-}
+})

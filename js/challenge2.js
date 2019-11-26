4dd791466dfa697fbd060112c38250d499f1f107
sophjac <33553357+sophjac@users.noreply.github.com>
2019-11-25 23:33:54 -0500

    Add files via upload

--- js/challenge2.js
+++ js/challenge2.js
@@ -1,11 +1,22 @@
-useBilling.onchange = function() {
-    let home = document.querySelector("#home");
-    if (this.checked) {
-        home.value = document.querySelector("#billing").value;
-        home.disabled = true;
-    } else {
-        home.value = "";
-        home.disabled = false;
-    }
+// useBilling.onchange = function() {
+//     let home = document.querySelector("#home");
+//     if (this.checked) {
+//         home.value = document.querySelector("#billing").value;
+//         home.disabled = true;
+//     } else {
+//         home.value = "";
+//         home.disabled = false;
+//     }
 
-}
+// }
+var checked = 0;
+
+$("#useBilling").click(function() {
+    if (checked == 0) {
+        $("#home").val($("#billing").val());
+        checked = 1;
+    } else {
+        $("#home").val("");
+        checked = 0;
+    }
+});
